0567e7b642833e21bbce91d2e37706cffb4052c1
Ceyda Alpay <44135146+ceydalpay@users.noreply.github.com>
2021-12-10 10:22:38 +0300

    Add files via upload

---
+++ src/components/GuestItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="guest-item" v-bind:class="{'is-complete':guest.completed}">
+      <p>
+        Email: {{guest.email}}
+        <br>Message: {{guest.message}}
+        <button @click="$emit('del-guest', guest.id)" class="del">x</button>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GuestItem",
+  props: ["guest", "guests", "email", "id"]
+};
+</script>
+
+<style scoped>
+.guest-item {
+  background: #f4f4f4;
+  padding: 10px;
+  margin: 20px;
+  border-bottom: 1px #ccc dotted;
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del {
+  background: #e75656ec;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
---
+++ src/components/Guests.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-bind:key="guest.id" v-for="guest in guests">
+      <GuestItem v-bind:id="guest.id" v-bind:email="guest.email" v-bind:message="guest.message"
+       v-bind:guest="guest" v-on:del-guest="$emit('del-guest', guest.id)"
+       v-bind:guests="guests"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import GuestItem from './GuestItem.vue';
+export default {
+  name: "Guests",
+  components: {
+    GuestItem
+  },
+  props: ["guests"]
+}
+</script>
+
+<style scoped>
+</style>
