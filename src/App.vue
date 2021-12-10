e6c7181c160db28f4f7bcc3fe19167a67a1680a3
Ceyda Alpay <44135146+ceydalpay@users.noreply.github.com>
2021-12-10 10:20:32 +0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <AddGuest v-on:add-guest="addGuest"/>
+    <Guests v-bind:guests="guests" v-on:del-guest="deleteGuest"/>
+  </div>
+</template>
+
+<script>
+import Guests from './components/Guests';
+import AddGuest from './components/AddGuest'
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      guests:[
+        
+      ]
+    }
+  },
+  components: {
+    Guests,
+    AddGuest
+  },
+  methods:{
+
+    addGuest(newGuest){
+      this.guests = [...this.guests,newGuest]
+    },
+    deleteGuest(id){
+      this.guests = this.guests.filter((guest) => guest.id !== id)
+    }
+  }
+};
+</script>
